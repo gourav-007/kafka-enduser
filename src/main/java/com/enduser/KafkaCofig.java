fa64f27d535f4c570ba0c13ef51c6ad8fa90f4a0
gourav-007 <gourav071295@gmail.com>
2024-07-06 20:18:32 +0530

    kaka-enduser

--- src/main/java/com/enduser/KafkaCofig.java
+++ src/main/java/com/enduser/KafkaCofig.java
@@ -5,7 +5,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 @Configuration
 public class KafkaCofig {
-
     @KafkaListener(topics=AppConstants.LOCATION_UPDATE_TOPIC,groupId=AppConstants.GROUP_ID)
     public void updatedLocation(String value){
         System.out.println(value);
